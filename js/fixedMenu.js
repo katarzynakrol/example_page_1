185201c664ece59845556e5fd60577119e278fef
katarzynaarmatys <armatys.k@gmail.com>
2017-07-17 21:07:16 +0200

    Utworzenie repozytorium podstrony pierwszej

---
+++ js/fixedMenu.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+  function fixDiv() {
+    var $cache = $('#main-nav');
+    if ($(window).scrollTop() > 100)
+      $cache.css({
+        'position': 'fixed',
+        'top': '0px',
+        'width': '100%'
+      });
+    else
+      $cache.css({
+        'position': 'relative',
+        'top': 'auto'
+      });
+  }
+  $(window).scroll(fixDiv);
+  fixDiv();
+});
